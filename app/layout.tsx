468575e24652857c9107c96bceac8790ae240f54
Samir Aoulad Amar <aouladamarsamirr2@gmail.com>
2025-12-23 21:50:43 +0100

    foat(UI,DB):create interface about home page and dashboar and add database src

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans antialiased`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`font-sans antialiased`} suppressHydrationWarning>
+
         {children}
         <Analytics />
       </body>
