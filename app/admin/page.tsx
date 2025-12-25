0e128b701bbc963c586f0e4ca8d1ede7289871c4
Samir Aoulad Amar <aouladamarsamirr2@gmail.com>
2025-12-25 17:40:49 +0100

    foat(UI,DB):create interface about home page and dashboar and add database src

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,32 @@
+"use client"
+
+import Navigation from "@/components/navigation"
+import Hero from "@/components/hero"
+import CategoriesGrid from "@/components/categories-grid"
+import Footer from "@/components/footer"
+import RandomItemsScroll from "@/components/random-items-scroll"
+import CategoryCharts from "@/components/category-charts"
+
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-[rgba(0,0,0,0.5)]  relative">
+            <Navigation />
+            <Hero />
+            <main className="container mx-auto px-4  " style={{ paddingTop: "15px" }}>
+                <CategoriesGrid />
+            </main>
+
+            {/* Charts Section */}
+            <CategoryCharts />
+
+            {/* Infinite Horizontal Scroll of Random Items */}
+            <RandomItemsScroll />
+
+            <Footer />
+        </div>
+    )
+}
+
 // "use client"
 
 // import { useAuth } from "@/contexts/auth-context"
